52c4e4dde564ccb9ac7076076600ae898bc0d07e
KatAnn15 <katherineviegh@gmail.com>
2021-09-18 21:21:38 +0300

    server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8080;
+const app = express();
+const compression = require('compression');
+
+app.use(compression());
+app.use(express.static(__dirname + '/dist'));
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "dist/index.html"));
+    console.log("server running!")
+})
+
+app.listen(port);
